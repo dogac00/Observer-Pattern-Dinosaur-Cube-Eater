5ddb0f8aae578488e29c4c8fbf77f1618ba1eeea
Dogac <ndakyildiz@gmail.com>
2019-08-25 20:55:15 +0300

    Project Committed.

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public static event Action<GameObject> OnCube;
+
+    void Awake()
+    {
+        OnCube = delegate { };
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        OnCube(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/CubeEater.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeEater : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cube.OnCube += CubeEater_OnCube;
+    }
+
+    private void CubeEater_OnCube(GameObject gameObject)
+    {
+        Destroy(gameObject);
+    }
+}
